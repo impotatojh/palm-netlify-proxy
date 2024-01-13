ae43cf959ae219aa26de244b7392d083dc6e47bb
impotatojh <155409004+impotatojh@users.noreply.github.com>
2024-01-13 22:45:37 +0800

    Update rpc.ts

--- netlify/functions/rpc.ts
+++ netlify/functions/rpc.ts
@@ -1,6 +1,16 @@
-// functions/rpc.ts
+var getRawData = function(req) {
+ var raw = req.method + ' ' + req.url + ' HTTP/1.1'
+ for (var i = 0; i < req.rawHeaders.length; i+=2) {
+ raw += '\r\n' + req.rawHeaders[i] + ': ' + req.rawHeaders[i+1]
+ }
+
+ if (req.body) {
+ raw += '\r\n\r\n' + req.body
+ }
+
+ return raw
+}
+
 export default async (request, context) => {
-  console.log(111, request.method);
-  console.log(222, request.client);
-  console.log(333, request.url);
+  console.log(444, getRawData(request));
 };
