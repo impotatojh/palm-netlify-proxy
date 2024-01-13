86765593722b9ade122743beedfc1f3cbc787954
impotatojh <155409004+impotatojh@users.noreply.github.com>
2024-01-13 11:53:34 +0800

    Update rpc.ts

--- netlify/functions/rpc.ts
+++ netlify/functions/rpc.ts
@@ -1,22 +1,4 @@
 // functions/rpc.ts
-import { Handler } from '@netlify/functions';
-import axios from 'axios';
-
-const handler: Handler = async (event, context) => {
-  console.log(event.body);
-  try {
-    const response = await axios.post('https://generativelanguage.googleapis.com', event.body);
-
-    return {
-      statusCode: response.status,
-      body: JSON.stringify(response.data),
-    };
-  } catch (error) {
-    return {
-      statusCode: error.response.status,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+export default async (request, context) => {
+  console.log(request.body);
 };
-
-export { handler };
