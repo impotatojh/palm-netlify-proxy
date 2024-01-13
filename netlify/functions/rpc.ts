f1ad39177eb9fa5f77727c0db4d28018f5491be9
impotatojh <155409004+impotatojh@users.noreply.github.com>
2024-01-13 12:17:13 +0800

    Update rpc.ts

--- netlify/functions/rpc.ts
+++ netlify/functions/rpc.ts
@@ -1,4 +1,6 @@
 // functions/rpc.ts
 export default async (request, context) => {
-  console.log(request.state.client);
+  console.log(111, request.method);
+  console.log(222, request.client);
+  console.log(333, request.url);
 };
